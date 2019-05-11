8e338431bcf31523b945adad7170aabd6a4529ad
Carolyn McNeillie <cmcneillie@theworkinggroup.ca>
2019-05-11 14:50:56 -0400

    scss & getting canvas started

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,31 +5,18 @@
  */
 
 module.exports = {
-  siteMetadata: 
-    {
-      title: `Paint your heart out`,
-      description: `Noodling around with HTML Canvas`,
-    }, 
   plugins: [
-    `gatsby-plugin-sass`, 
+    `gatsby-plugin-sass`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
       options: {
-        name: "GatsbyJS",
-        short_name: "GatsbyJS",
-        start_url: "/",
-        background_color: "#6b37bf",
-        theme_color: "#6b37bf",
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: "standalone",
-        icon: "src/images/icon.png", // This path is relative to the root of the site.
-        // An optional attribute which provides support for CORS check.
-        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
-        // Any invalid keyword or empty string defaults to `anonymous`
-        crossOrigin: `use-credentials`,
+        fonts: [
+          {
+            family: `VT323`,
+            subsets: [`latin`],
+          }
+        ],
       },
-    },
-    'gatsby-plugin-offline'
+    }
   ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,9 @@
 import React from "react"
-import { Link } from "gatsby"
 import Header from "../components/header"
 
 export default () => (
-  <div style={{ color: `purple` }}>
-    <Link to="/contact/">Contact</Link>
-    <Header headerText="Hello world" />
-    <p>What a world.</p>
-    <img src="https://source.unsplash.com/random/400x200"
-    alt = "" />
+  <div className="window">
+    <Header headerText="Paintbrush - (Untitled)" />
+    <canvas id="canvas"></canvas>
   </div>
 )
